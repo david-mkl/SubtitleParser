9e13b6ecb0e5503d530144e68421a8a205d943ec
David Lawrence <32286773+david-mk-lawrence@users.noreply.github.com>
2022-07-04 16:04:55 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SubtitleParser
+
+A description of this package.
---
+++ Sources/SubtitleParser/SubtitleParser.swift
@@ -0,0 +1,6 @@
+public struct SubtitleParser {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
